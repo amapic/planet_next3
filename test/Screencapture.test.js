1e78a2f3ce38693e46575641dfbbae75f7fd21d1
amapic <amaury.pichat@gmail.com>
2023-12-02 01:11:53 +0100

    ajout test simple

---
+++ test/Screencapture.test.js
@@ -0,0 +1,19 @@
+
+
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
+const puppeteer = require('puppeteer');
+expect.extend({ toMatchImageSnapshot });
+
+
+// it('CreateReactApp home', async () => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto('http://localhost:4000/screenshot?url=https://example.com/');
+//     const image = await page.screenshot();
+
+//     expect(image).toMatchImageSnapshot();
+// },50000)
+
+it('CreateReactApp home', async () => {
+  expect("1").toEqual("1");
+})
