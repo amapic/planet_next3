ee485affab85e41d7e802644aff2371fd0e56803
amaurypichat <amopichat@yahoo.fr>
2023-12-11 00:09:57 +0100

    chore: update tsconfig.json

--- test/gg.te_st.js
+++ test/gg.te_st.js
@@ -11,28 +11,9 @@ import React from "react";
 // jest.mock('@react-three/cannon');
 
 test("Texte centrale", async () => {
-  // const useEffectMock = jest.fn();
-  // jest.spyOn(console, "warn").mockImplementation(() => {});
-
-  // jest.spyOn(React, "useEffect").mockImplementation(useEffectMock);
-  // ARRANGE
   var hh = "";
-  // act(() => {
   const result = render(<PanelGauche />);
-  // hh = result.container.querySelector("#pannfgifeau");
-  // });
-
-  // ACT
-  //   await userEvent.click(screen.getByText('Load Greeting'))
-  // .container.querySelector('#some-id');
 
   await screen.findByRole("contact");
-  // await screen.findByRole("contact");
-
-  // ASSERT
-  // expect(hh).toHaveTextContent(
-  //   "Cliquez sur les fleches pour faire defiler les systemes solaires"
-  // );
   expect(screen.getByRole("contact")).toHaveTextContent(Array(7).join("Cliquez sur les fleches pour faire defiler les systemes solaires et cliquez sur une planete pour avoir des informations la concernant"));
-  //   expect(screen.getByRole('button')).toBeDisabled()
 });
