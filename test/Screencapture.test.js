d793dc6d9c7921c17be04587e2c10ed0a9498dae
amaurypichat <amopichat@yahoo.fr>
2023-12-11 15:27:05 +0100

    test: update Screencapture.test.js

--- test/Screencapture.test.js
+++ test/Screencapture.test.js
@@ -12,7 +12,7 @@ function sleep(ms) {
 
 it("CreateReactApp home", async () => {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     // product: "chrome",
 
     // args: ["--no-sandbox","--disable-setuid-sandbox"],
