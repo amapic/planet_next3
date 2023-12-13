e1e4f3997fbed185a9b2de71c16fcfb2dcaf79c6
amaurypichat <amopichat@yahoo.fr>
2023-12-13 19:57:31 +0100

    test: update Screencapture.test.js

--- test/Screencapture.test.js
+++ test/Screencapture.test.js
@@ -17,9 +17,9 @@ it("CreateReactApp home", async () => {
 
     // args: ["--no-sandbox","--disable-setuid-sandbox"],
     args: ["--no-sandbox"],
-    executablePath: process.env.PUPPETEER_EXEC_PATH,
+    // executablePath: process.env.PUPPETEER_EXEC_PATH,
     defaultViewport: { width: 1700, height: 800 },
-    slowMo: 50
+    // slowMo: 50
   });
   const page = await browser.newPage();
   await page.goto("https://amaurypichat.fr/dev/planet", {
