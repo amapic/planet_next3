1932cf46ed3f6ab1eaa2ed9b9fda06189e7a714a
amaurypichat <amopichat@yahoo.fr>
2023-12-15 18:17:41 +0100

    update 1 file and delete 1 file

---
+++ ca_marche.tes_t.js
@@ -0,0 +1,66 @@
+/*
+ * @jest-environment node
+ */
+
+import { toMatchImageSnapshot } from "jest-image-snapshot";
+import puppeteer from "puppeteer";
+expect.extend({ toMatchImageSnapshot });
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+it("CreateReactApp home", async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    // product: "chrome",
+
+    // args: ["--no-sandbox","--disable-setuid-sandbox"],
+    args: ["--no-sandbox"],
+    executablePath: process.env.PUPPETEER_EXEC_PATH,
+    defaultViewport: { width: 1700, height: 800 },
+    slowMo: 50
+  });
+  const page = await browser.newPage();
+  await page.goto("https://amaurypichat.fr/dev/planet", {
+    // waitUntil: "networkidle0",
+  });
+
+  //   // await page.waitForTimeout(6000);
+  //   // sleep(20000).then(async () => {
+  //   //   console.log("World!");
+  const image = await page.screenshot({
+    // clip: { x: 220, y: 0, width: 630, height: 360 },
+  });
+
+  expect(image).toMatchImageSnapshot({
+    // comparisonMethod: 'ssim',
+    failureThreshold: 5,
+    failureThresholdType: 'percent'
+  });
+
+  await browser.close();
+}, 100000);
+
+// sleep(30000).then(async () => {
+// const element = await page.waitForSelector("#div_canvas1");
+
+// await element.click();
+
+// console.log("World!");
+// const image = await page.screenshot();
+
+// expect(image).toMatchImageSnapshot({
+//   failureThreshold: 0,
+//   failureThresholdType: "percent"
+// });
+
+// await browser.close();
+// });
+
+// await browser.close();
+// }, 100000);
+
+// it('CreateReactApp home', async () => {
+//   expect("2").toEqual("1");
+// })
--- test/api_vision_kimarche.test.js
+++ test/api_vision_kimarche.test.js
@@ -14,18 +14,6 @@ import "@testing-library/jest-dom";
 // import PanelGauche from "../components_planet/PanneauMobile";
 import React from "react";
 
-// const setup = () => {
-// return render(Home);
-// };
-// jest.mock('@react-three/cannon');
-
-// test("Texte centrale", async () => {
-//   var hh = "";
-//   const result = render(<PanelGauche />);
-
-//   await screen.findByRole("contact");
-//   expect(screen.getByRole("contact")).toHaveTextContent(Array(7).join("Cliquez sur les fleches pour faire defiler les systemes solaires et cliquez sur une planete pour avoir des informations la concernant"));
-// });
 
 test("Texte centrale", async () => {
   // Creates a client
